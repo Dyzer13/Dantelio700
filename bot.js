76703fe0589edce11c49a9b9e435b57baaea5db4
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-08-12 10:12:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -267,6 +267,20 @@ client.on('message', function(message) {
 });
 
 
+client.on("message", (message) => {
+            if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        let yumz = new Discord.RichEmbed()
+                    .setTimestamp()
+                    .setTitle("Direct Message To The Bot")
+                    .addField(`Sent By:`, `<@${message.author.id}>`)
+                    .setColor("RANDOM")
+                    .setThumbnail(message.author.displayAvatarURL)
+                    .addField(`Message: `, `\n\n\`\`\`${message.content}\`\`\``)
+                    .setFooter(`DM Bot Messages | DM Logs`)
+                client.users.get("471788143637430273").send(yumz)
+            }
+});
 
 
 
