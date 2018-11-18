b2ae9d70a944f29c9a66c40c4afe3e47335169d7
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-18 23:37:16 +0100

    Create bot5.js

---
+++ bot5.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const adminprefix = "-";
+const devs = ['513323053334396928'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+      
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
+
+client.login(process.env.BOT_TOKEN5);
