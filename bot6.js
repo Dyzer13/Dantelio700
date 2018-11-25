52f1ab28fd3de3ecfbe3e37e1c42b59d451275be
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-25 22:06:31 +0100

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -1,16 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const adminprefix = "-";
-const devs = ['437598889881894912'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-      
-if (message.content.startsWith(adminprefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش حب:white_check_mark:   ${argresult}**`)
-}
-});
 
 client.login(process.env.BOT_TOKEN6);
