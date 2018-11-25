8fa5652a9c4063881a4facf0e18fd7a879341b33
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-25 22:10:16 +0100

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -1,5 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on("message", (message) => {
+            if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        let yumz = new Discord.RichEmbed()
+                    .setTimestamp()
+                    .setTitle("Direct Message To Michou")
+                    .addField(`Sent By:`, `<@${message.author.id}>`)
+                    .setThumbnail(message.author.displayAvatarURL)
+                    .addField(`Message: `, `\n\n\`\`\`${message.content}\`\`\``)
+                client.users.get("515474180603641866").send(yumz)
+            }
+});
 
 client.login(process.env.BOT_TOKEN6);
