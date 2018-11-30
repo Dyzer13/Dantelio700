3f914296b6244756d756207bab66c7974c484de6
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-30 11:46:09 +0100

    Create bot6.js

---
+++ bot6.js
@@ -0,0 +1,16 @@
+const client = new Discord.Client();
+
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('-bc-users')){
+if(!message.author.id === '515474180603641866') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+client.login(process.env.BOT_TOKEN6);
