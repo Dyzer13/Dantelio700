bee0451d6322d682d1ad09726d72bb35b7670050
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-30 11:47:23 +0100

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('-bc-users')){
+if (message.content.startsWith('')){
 if(!message.author.id === '515474180603641866') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
